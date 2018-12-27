dbb3bf521772e0b43362a323cac9eba3285ba190
畅界 <changjie@changjiedeMacBook-Pro.local>
2018-12-27 19:20:27 +0800

    sub

--- Example/Pods/Target Support Files/Distinguish/Distinguish-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Distinguish : NSObject
-@end
-@implementation PodsDummy_Distinguish
-@end
--- Example/Pods/Target Support Files/Pods-Distinguish_Example/Pods-Distinguish_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Distinguish_Example : NSObject
-@end
-@implementation PodsDummy_Pods_Distinguish_Example
-@end
